5ab03d5de2009975b4d0c135ac20a0f22fd64523
TanmayYadavCS <sankalp.s.yadav@outlook.com>
2024-07-07 20:23:05 -0500

    added smoothing and other cleanup operations

--- Engine.py
+++ Engine.py
@@ -1,3 +1,4 @@
+import math
 class Value:
     """Created by Tanmay Yadav on July 7, 2024
     > Store a singular scalar and its gradients. 
@@ -49,5 +50,82 @@ class Value:
         # call it
         out._backward = _backward
         return out
-
     
+    def exp(self):
+        """Returns value processed through the natural exponential function."""
+        out = Value(math.exp(self.data), (self,), 'e')
+
+        # backprop
+        def _backward():
+            self.grad += math.exp(self.data) * out.grad
+        # call it
+        out._backward = _backward
+        return out
+
+    def relu(self):
+        """Returns value processed through the ReLU smoothing function."""
+        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+
+        # backprop
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+        # call it
+        out._backward = _backward
+        return out
+    
+    def sigmoid(self):
+        """Returns value processed through the Sigmoid smoothing function."""
+        out = Value(1 / (1 + self.exp(-1*self.data)), (self,), 'Sigmoid')
+
+        # backprop
+        def _backward():
+            self.grad += self.sigmoid(self.data) * (1 - self.sigmoid(self.data)) * out.grad
+        # call it
+        out._backward = _backward
+        return out
+    
+    def backward(self):
+        """Initiates the backward propogation from the parent node."""
+
+        # topological sort
+        topo = []
+        visited = set()
+        
+        def build_topological(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topological(child)
+                topo.append(v)
+        
+        build_topological(self)
+
+        # go one variable at a time and apply the chain rule to get the subsequent gradients.
+        self.grad = 1
+        # since we will start with the last parent node
+        for v in reversed(topo):
+            v._backward()
+    
+    def __neg__(self): # -self
+        return self * -1
+
+    def __radd__(self, other): # other + self
+        return self + other
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __rsub__(self, other): # other - self
+        return other + (-self)
+
+    def __rmul__(self, other): # other * self
+        return self * other
+
+    def __truediv__(self, other): # self / other
+        return self * other**-1
+
+    def __rtruediv__(self, other): # other / self
+        return other * self**-1
+
+    def __repr__(self):
+        return f"Value(data = {self.data}, grad = {self.grad})"
